f0cbb441bb85437403944a3fd4a65bbc0b69f11f
Yehezkiel David Setiawan <90822994+YehezkielDavid06@users.noreply.github.com>
2022-02-16 23:22:19 +0700

    Add files via upload

--- Buat Lomba/script/Home.js
+++ Buat Lomba/script/Home.js
@@ -1,9 +1,26 @@
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 50) {
-    nav.classList.add('bg-secondary', 'shadow');
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-info', 'shadow');
   } else {
-    nav.classList.remove('bg-secondary', 'shadow');
+    nav.classList.remove('bg-info', 'shadow');
   }
+});
+
+$('.counter-count').each(function () {
+  $(this)
+    .prop('Counter', 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
 });
