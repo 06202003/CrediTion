0b165c0eac126bd934e1214fddd3cf886402992e
Arthurito <90821581+0Arthexx@users.noreply.github.com>
2022-02-18 21:14:41 +0700

    Add files via upload

--- script/Home.js
+++ script/Home.js
@@ -1,10 +1,10 @@
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 100) {
-    nav.classList.add('bg-info', 'shadow');
+  if (window.pageYOffset > 50) {
+    nav.classList.add('warnaheader', 'shadow');
   } else {
-    nav.classList.remove('bg-info', 'shadow');
+    nav.classList.remove('warnaheader', 'shadow');
   }
 });
 
@@ -23,4 +23,36 @@ $('.counter-count').each(function () {
         },
       }
     );
+});
+
+
+// animasi
+let tl = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 1000,
+  loop: true,
+  endDelay: 500
+}); 
+
+tl.add({
+  targets: '.foxhead',
+  scale: [0.75, 1],
+  opacity: [0, 1],
+})
+.add({
+  targets: '.foxpart',
+  opacity: [0, 1],
+  delay: anime.stagger(200),
+  easing: 'easeInExpo',
+}, '-=2000')
+.add({
+  targets: '#name-g path',
+  opacity: [0, 1],
+  translateX: [-25, 0],
+  rotate: ['-45deg', 0],
+  delay: anime.stagger(100),
+}, '-=500')
+.add({
+  targets: 'svg',
+  opacity: [1, 0],
 });
